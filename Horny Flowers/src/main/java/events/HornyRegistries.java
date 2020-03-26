41a0cd31bd3f48c90541730333090419eb0e0a8e
misterghast <51974455+misterghast@users.noreply.github.com>
2020-03-26 23:09:48 +0000

    Add files via upload

---
+++ Horny Flowers/src/main/java/events/HornyRegistries.java
@@ -0,0 +1,46 @@
+package events;
+
+import ghast.hornyflowers.HornyFlowers;
+import ghast.hornyflowers.items.GrainHorn;
+import net.minecraft.block.Block;
+import net.minecraft.item.Item;
+import net.minecraft.item.Item.Properties;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.client.event.ModelRegistryEvent;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.registries.IForgeRegistryEntry;
+
+@Mod.EventBusSubscriber(bus=Mod.EventBusSubscriber.Bus.MOD)
+public class HornyRegistries {
+	public static Properties defaultProperties(int stackSize) {
+		return new Properties().maxStackSize(stackSize).setNoRepair();
+	}
+	
+    @SubscribeEvent
+    public static void onBlocksRegistry(final RegistryEvent.Register<Block> blockRegistryEvent) {
+  
+    }
+    
+    @SubscribeEvent
+    public static void onItemsRegistry(final RegistryEvent.Register<Item> event) {
+    	GrainHorn horn = new GrainHorn(defaultProperties(1));
+    	event.getRegistry().register(setup(horn, "grainhorn"));
+    }
+    
+    @SubscribeEvent
+    public static void onModelRegistry(final ModelRegistryEvent event) {
+
+    }
+    
+    public static <T extends IForgeRegistryEntry<T>> T setup(final T entry, final String name) {
+    	return setup(entry, new ResourceLocation(HornyFlowers.MODID, name));
+    }
+
+    public static <T extends IForgeRegistryEntry<T>> T setup(final T entry, final ResourceLocation registryName) {
+    	entry.setRegistryName(registryName);
+    	return entry;
+    }
+
+}
