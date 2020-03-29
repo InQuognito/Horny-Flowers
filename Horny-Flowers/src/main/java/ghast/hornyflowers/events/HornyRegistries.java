97bcc77c05e63d6aa6833336684cb8e3d7ace472
stretch <stretch@icelan.com>
2020-03-29 15:19:30 -0500

    Added item tab and localization fixes.

--- Horny-Flowers/src/main/java/ghast/hornyflowers/events/HornyRegistries.java
+++ Horny-Flowers/src/main/java/ghast/hornyflowers/events/HornyRegistries.java
@@ -2,6 +2,7 @@ package ghast.hornyflowers.events;
 
 import ghast.hornyflowers.HornyFlowers;
 import ghast.hornyflowers.items.GrainHorn;
+import ghast.hornyflowers.items.HornyItemGroup;
 import net.minecraft.block.Block;
 import net.minecraft.item.Item;
 import net.minecraft.item.Item.Properties;
@@ -11,11 +12,12 @@ import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.registries.IForgeRegistryEntry;
+import vazkii.botania.common.core.BotaniaCreativeTab;
 
 @Mod.EventBusSubscriber(bus=Mod.EventBusSubscriber.Bus.MOD)
 public class HornyRegistries {
 	public static Properties defaultProperties(int stackSize) {
-		return new Properties().maxStackSize(stackSize).setNoRepair();
+		return new Properties().maxStackSize(stackSize).setNoRepair().group(HornyItemGroup.INSTANCE);
 	}
 	
     @SubscribeEvent
---
+++ Horny-Flowers/src/main/java/ghast/hornyflowers/items/StaticItems.java
@@ -0,0 +1,9 @@
+package ghast.hornyflowers.items;
+
+import ghast.hornyflowers.HornyFlowers;
+import net.minecraftforge.registries.ObjectHolder;
+
+@ObjectHolder(HornyFlowers.MODID)
+public class StaticItems {
+	public static final GrainHorn HORN_GRAIN = null;
+}
